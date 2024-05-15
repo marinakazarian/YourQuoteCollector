32d50cd18afcb510cba40b32c8729241db6398a1
Marina <mkazarian@g.hmc.edu>
2024-05-15 15:17:59 -0400

    debugging

--- background.js
+++ background.js
@@ -1,7 +1,19 @@
-chrome.contextMenus.create({
-  title: "Add to Quote List",
-  contexts: ["selection"],
-  onclick: function(info, tab) {
-    chrome.tabs.sendMessage(tab.id, {action: "addQuote", selection: info.selectionText});
-  }
-});
+// Background script - background.js
+
+// Listen for context menu item clicks
+chrome.contextMenus.onClicked.addListener((info, tab) => {
+    // Check if the clicked menu item is the one we added
+    if (info.menuItemId === "addToQuoteList") {
+      // Open a new tab with the Wikipedia page for the highlighted text
+      chrome.tabs.sendMessage(tab.id, {action: "addQuote", selection: info.selectionText});
+    }
+  });
+  
+  // Create context menu item
+  chrome.runtime.onInstalled.addListener(() => {
+    chrome.contextMenus.create({
+      id: "addToQuoteList",
+      title: "Add to Quote List",
+      contexts: ["selection"],
+    });
+  });
