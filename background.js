2aeaab9cc12529f349a85e9335dcd95e08285efd
Marina <mkazarian@g.hmc.edu>
2024-05-15 16:51:51 -0400

    changed some stylistic elements

--- background.js
+++ background.js
@@ -8,7 +8,7 @@ chrome.runtime.onInstalled.addListener(() => {
 
 chrome.contextMenus.onClicked.addListener((info, tab) => {
   if (info.menuItemId === 'addQuote') {
-    saveQuote(info.selectionText);
+    saveQuote(concat("\"", info.selectionText, "\""));
   }
 });
 
