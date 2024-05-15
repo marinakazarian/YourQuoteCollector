0863cb14291b919f0acd4321462fafc16dc13171
Marina <mkazarian@g.hmc.edu>
2024-05-15 15:32:37 -0400

    debugging

--- background.js
+++ background.js
@@ -1,18 +1,8 @@
 let quotes = [];
 
 chrome.runtime.onMessage.addListener(function(message, sender, sendResponse) {
-  if (message.action === "checkContextMenu") {
-    chrome.contextMenus.create({
-      id: "addQuoteOption",
-      title: "Add to Quote List",
-      contexts: ["selection"]
-    });
-  }
-});
-
-chrome.contextMenus.onClicked.addListener(function(info, tab) {
-  if (info.menuItemId === "addQuoteOption") {
-    let quote = info.selectionText;
+  if (message.action === "addQuote") {
+    let quote = message.quote;
     quotes.push(quote);
     chrome.storage.local.set({quotes: quotes}, function() {
       console.log("Quote added: " + quote);
--- content.js
+++ content.js
@@ -1,6 +1,7 @@
 document.addEventListener("contextmenu", function(event) {
     let selectedText = window.getSelection().toString().trim();
     if (selectedText !== "") {
-        chrome.runtime.sendMessage({action: "checkContextMenu"});
+      chrome.runtime.sendMessage({action: "checkContextMenu"});
     }
-});
+  });
+  
