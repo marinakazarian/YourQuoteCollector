88977cabdd44bf2ae6bbea83cdb4e286d4f303f9
Marina <mkazarian@g.hmc.edu>
2024-05-15 15:29:13 -0400

    add quote only on right click

--- background.js
+++ background.js
@@ -1,8 +1,18 @@
 let quotes = [];
 
 chrome.runtime.onMessage.addListener(function(message, sender, sendResponse) {
-  if (message.action === "addQuote") {
-    let quote = message.quote;
+  if (message.action === "checkContextMenu") {
+    chrome.contextMenus.create({
+      id: "addQuoteOption",
+      title: "Add to Quote List",
+      contexts: ["selection"]
+    });
+  }
+});
+
+chrome.contextMenus.onClicked.addListener(function(info, tab) {
+  if (info.menuItemId === "addQuoteOption") {
+    let quote = info.selectionText;
     quotes.push(quote);
     chrome.storage.local.set({quotes: quotes}, function() {
       console.log("Quote added: " + quote);
--- content.js
+++ content.js
@@ -1,7 +1,6 @@
-document.addEventListener("mouseup", function(event) {
-    var selectedText = window.getSelection().toString().trim();
+document.addEventListener("contextmenu", function(event) {
+    let selectedText = window.getSelection().toString().trim();
     if (selectedText !== "") {
-      chrome.runtime.sendMessage({action: "addQuote", quote: selectedText});
+        chrome.runtime.sendMessage({action: "checkContextMenu"});
     }
-  });
-  
+});
